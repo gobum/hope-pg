1e3ec688a5448d4b4ca05bad362b16d1710b74d0
leadzen <leadzen@qq.com>
2023-08-25 06:48:58 +0000

    优化

--- hope.sql
+++ hope.sql
@@ -51,10 +51,10 @@
 -- :ASS     当前测试用例用于测试断言
 -- :VAR     当前测试用于设置变量
 
-\set to_job '\\set error 0 \\set JOB 1 \\set SQL 1 \\set DID 0 \\set ACT 0 \\set ROW 0 \\set COL 0 \\set ASS 0 '
+\set to_job '\\set JOB 1 \\set END 0 \\set SQL 1 \\set DID 0 \\set ACT 0 \\set ROW 0 \\set COL 0 \\set ASS 0 '
 \set to_act '\\set ACT 1 '
 \set to_did '\\set DID 1 \\set ACT 1'
-\set to_end '\\set error 0 \\set JOB 0 \\set SQL 1 \\set DID 1 \\set ACT 1 \\set ROW 0 \\set COL 0 \\set ASS 0 '
+\set to_end '\\set END 1 \\set JOB 0 \\set SQL 1 \\set DID 1 \\set ACT 1 \\set ROW 0 \\set COL 0 \\set ASS 0 '
 :to_end
 
 ------------------------------------------------------------------------------------------------
@@ -65,56 +65,57 @@
 \set SQL_NONE '\\set ECHO none\\\\' \\--打开 SQL 语句自身输出
 
 \set log '\\echo -n '
-\set err '\\set error 1 \\echo -n ' :y
+\set err '\\echo -n ' :y
 
 \set cmd '\\if :SQL ' :log:b ' \\p ' :log:z ' \\set SQL 0 \\endif '
 
 \set exe '\\if :ACT \\r \\else ' :t1 :log:d '\\g' :log:z :t0 ' \\endif '
 \set des :t1 :log:d '\\gdesc ' :log:z :t0
 \set gxe :t1 :log:d '\\set ROW_COUNT 0 \\gset' :log:z :t0
-\set row :exe ' \\\\select :ROW_COUNT:EXPR as assert \\gset '
-\set col :des ' \\\\select :ROW_COUNT:EXPR as assert \\gset '
-\set ass :gxe '\\\\ select :ROW_COUNT <>1 error \\gset '
+\set cnt ' \\\\select :ROW_COUNT:EXPR as assert \\gset '
+\set row :exe :cnt
+\set col :des :cnt
+\set one :gxe '\\\\ select :ROW_COUNT <>1 error \\gset '
 \set do_pass :cmd :exe ' \\if :ERROR ' :log :f:s_pass:z ' \\else ' :log :t:s_pass:d ' \\endif\\\\'
 \set do_fail :cmd :exe ' \\if :ERROR ' :log :t:s_fail:z ' \\else ' :log :f:s_fail:d ' \\endif\\\\'
 \set do_row '\\if :ERROR ' :log :u:s_rows:z ' \\else ' :row ' \\if :ERROR ' :log :u:s_rows:z ' \\elif :assert ' :log :t:s_rows:z ' \\else ' :log :f:s_rows:z '\\endif \\endif\\\\ '
 \set do_col '\\if :ERROR ' :log :u:s_cols:z ' \\else ' :col ' \\if :ERROR ' :log :u:s_cols:z ' \\elif :assert ' :log :t:s_cols:z ' \\else ' :log :f:s_cols:z '\\endif \\endif\\\\ '
-\set do_ass :cmd :ass ' \\if :error ' :log :u:s_assert:z ' \\elif :assert ' :log :t:s_assert:z ' \\else ' :log :f:s_assert:z ' \\endif\\\\' 
-\set do_var :cmd :ass '\\if :error ' :log :f:s_var:z ' \\else ' :log :t:s_var:z '\\endif\\\\'
+\set do_ass :cmd :one ' \\if :error ' :log :u:s_assert:z ' \\elif :assert ' :log :t:s_assert:z ' \\else ' :log :f:s_assert:z ' \\endif\\\\' 
+\set do_var :cmd :one '\\if :error ' :log :f:s_var:z ' \\else ' :log :t:s_var:z '\\endif\\\\'
 
 -- :Include 引用其他SQL文件
 \set Include '\\if :JOB ' :err :e_include:z ' \\q \\else ' :log :s_include ' \\endif \\ir '
 
 -- :Hope  开始一个测试用例 E_HOP ? WRONG : E_HOP->0, E_END->1, E_EXE->1, E_ASS->1, E_ROW->1, E_VAR->
-\set Hope '\\if :JOB ' :err :e_hope:z ' \\q \\elif :error \\else \\r ' :log :z:s_hope :to_job ' \\endif\\\\ ' 
+\set Hope '\\if :JOB ' :err :e_hope:z ' \\q \\endif' :log :z:s_hope :to_job '\\\\' 
 
 -- :Pass  断言 SQL 语句执行成功
-\set Pass '\\if :ACT ' :err :e_pass:z ' \\q \\else ' :do_pass :to_act ' \\endif\\\\'
+\set Pass '\\if :ACT ' :err :e_pass:z ' \\q \\endif' :do_pass :to_act '\\\\'
 
 -- :Fail  断言 SQL 语句执行失败
-\set Fail '\\if :ACT ' :err :e_fail:z ' \\q \\else ' :do_fail :to_act ' \\endif\\\\'
+\set Fail '\\if :ACT ' :err :e_fail:z ' \\q \\endif' :do_fail :to_act '\\\\'
 
 -- :Rows 记录行数断言
-\set Rows '\\if :DID ' :err :e_rows:z' \\q \\else ' :cmd :to_did ' \\set ROW 1 \\endif \\set EXPR '
+\set Rows '\\if :DID ' :err :e_rows:z' \\q \\endif' :cmd :to_did ' \\set ROW 1 \\set EXPR '
 
 -- :Columns 记录列数断言
-\set Columns '\\if :ACT ' :err :e_cols:z' \\q \\else ' :cmd :to_did ' \\set COL 1 \\endif \\set EXPR '
+\set Columns '\\if :ACT ' :err :e_cols:z' \\q \\endif' :cmd :to_did ' \\set COL 1 \\set EXPR '
 
 -- :Assert 断言表达式
-\set Assert '\\if :ACT ' :err :e_assert:z ' \\q \\else \\\\assert ' :to_did '\\set ASS 1 \\endif\\\\'
+\set Assert '\\if :ACT ' :err :e_assert:z ' \\q \\endif\\\\ assert ' :to_did '\\set ASS 1 \\\\'
 
 -- :Variable 设置变量
-\set Variable '\\if :ACT ' :err :e_var:z ' \\q \\else ' :do_var :to_did '\\endif\\\\'
+\set Variable '\\if :ACT ' :err :e_var:z ' \\q \\endif' :do_var :to_did '\\\\'
 
 -- :Done 结束测试用例
-\set Done '\\if :JOB \\if :ROW ' :do_row ' \\elif :COL ' :do_col ' \\elif :ASS ' :do_ass ' \\else ' :cmd :exe ' \\endif ' :log :z:s_done :to_end ' \\else ' :err :e_done:z ' \\q \\endif\\\\ '
+\set Done '\\if :END ' :err :e_done:z ' \\q \\endif \\if :ROW ' :do_row ' \\elif :COL ' :do_col ' \\elif :ASS ' :do_ass ' \\else ' :cmd :exe ' \\endif ' :log :z:s_done :to_end '\\\\'
 
 \o /dev/null
 
 :log 'Import hope.sql\n\n'
 
 \unset do_ass \unset do_row \unset do_col \unset do_fail \unset do_pass 
-\unset ass \unset row \unset col \unset gxe \unset des \unset exe \unset cmd \unset err \unset log
+\unset one \unset row \unset col \unset cnt \unset gxe \unset des \unset exe \unset cmd \unset err \unset log
 
 \unset t1 \unset t0 \unset to_end \unset to_did \unset to_act \unset to_job 
 \unset e_done \unset e_rows \unset e_cols \unset e_assert \unset e_fail \unset e_pass \unset e_hope \unset e_include
